1c8feb1abee605a2167f46b0b61e71a85bb00d3f
lumbers <noahlum2009@gmail.com>
2026-04-14 11:49:24 -0400

    fixed moving issue

--- Dodge It!/Hero.java
+++ Dodge It!/Hero.java
@@ -12,9 +12,11 @@ public class Hero extends Actor
      * Act - do whatever the Hero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    boolean atTop = true;
     public void act()
     {
-        boolean atTop = true;
+        
         
         // Add your action code here.
         if(Greenfoot.mouseClicked(null))
