6c50eb7a34314a3dfc8885df9bde80a91af7314e
lumbers <noahlum2009@gmail.com>
2026-04-14 11:40:53 -0400

    reset feetLover

--- Dodge It!/FeetLover.java
+++ Dodge It!/FeetLover.java
@@ -15,6 +15,24 @@ public class FeetLover extends Actor
     public void act()
     {
         // Add your action code here.
-        move(-3);
+        move(-10);
+        
+        if(getX() <= 0)
+        {
+            resetFeetLover();
+        }
+    }
+    
+    public void resetFeetLover()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600,100);
+        }
+        else
+        {
+            setLocation(600,300);
+        }
     }
 }
